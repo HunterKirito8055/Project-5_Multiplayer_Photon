bc14ec1a082a2d71a86348f831738ff217803a5c
SuperStarVishal <ineffable8055@gmail.com>
2022-01-16 12:05:28 +0530

    Revert "Networking photon serialised testing"
    This reverts commit 60901a572eb27dbeb654efe45be556c3fc4d3c52.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-using TMPro;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
@@ -11,20 +10,13 @@ public class GameManager : MonoBehaviourPunCallbacks
     public GameObject player;
     public Transform spawnPoint;
     public Button leaveRoomBtn;
-    public TextMeshProUGUI pingTxt;
     private void Start()
     {
-        PhotonNetwork.SerializationRate = 20;
         leaveRoomBtn.onClick.AddListener(LeaveRoom);
         sceneCam.gameObject.SetActive(false);
         if (player == null)
             player = Resources.Load("Player") as GameObject;
         PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation).SetActive(true); 
-    }
-
-    private void Update()
-    {
-        pingTxt.text = PhotonNetwork.GetPing().ToString();
     }
     /// <summary>
     /// Called when the local player left the room. We need to load the lobby scene.
